08a96433ec8bb7e1f291f87a8bf6268d0127f41a
Alexander <alex.dem.v@gmail.com>
2017-03-25 20:55:42 +0300

    add operators +, -, * to Task1

---
+++ Task1.Tests/PolynomialTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    class PolynomialTests
+    {
+        [Test]
+        public void Compute_12_457_Returns()
+        {
+            Polynomial a = new Polynomial(1, 2, 3);
+            double x = 12;
+
+            double expected = 457;
+
+            double actual = a.Compute(x);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void Compute_0_1_Returns()
+        {
+            Polynomial a = new Polynomial(1, 2, 3);
+            double x = 0;
+
+            double expected = 1;
+
+            double actual = a.Compute(x);
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ Task1/Polynomial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Polynomial: ICloneable
+    {
+        private double[] coefficients;
+
+        private int power;
+
+        public Polynomial(params double[] coefficients)
+        {
+            if (coefficients == null)
+                throw new ArgumentNullException(nameof(coefficients));
+
+
+
+            this.coefficients = coefficients;
+            power = coefficients.Length;
+        }
+
+        public object Clone()
+        {
+            return new Polynomial(this.coefficients);
+        }
+
+        public double Compute(double x)
+        {
+            double result = 0;
+
+            for (int i = power - 1; i >= 0; i--)
+                result = result * x + coefficients[i];
+
+            return result;
+        }
+
+        public static Polynomial operator +(Polynomial value1, Polynomial value2)
+        {
+            if (value1 == null && value2 == null)
+                return null;
+            if (value1 == null)
+                return new Polynomial(value2.coefficients);
+            if (value2 == null)
+                return new Polynomial(value1.coefficients);
+
+            double[] resultCoefficients = (value1.power > value2.power) ? 
+                (double[])value1.coefficients.Clone() : (double[])value2.coefficients.Clone();
+
+            int resultPower = (value1.power > value2.power) ? value2.power : value1.power;
+
+            for (int i = 0; i < resultPower; i++)
+                resultCoefficients[i] = value1.coefficients[i] + value2.coefficients[i];
+
+            return new Polynomial(resultCoefficients);
+        }
+
+        public static Polynomial operator -(Polynomial value1, Polynomial value2)
+        {
+            if (value1 == null && value2 == null)
+                return null;
+            if (value1 == null)
+                return new Polynomial(value2.coefficients);
+            if (value2 == null)
+                return new Polynomial(value1.coefficients);
+
+            return value1 + (-value2);
+        }
+
+        public static Polynomial operator -(Polynomial value)
+        {
+            if (value == null)
+                return null;
+
+            double[] resultCoefficients = new double[value.power + 1];
+            for (int i = 0; i < resultCoefficients.Length; i++)
+                resultCoefficients[i] = -value.coefficients[i];
+            return new Polynomial(resultCoefficients);
+        }
+
+        public static Polynomial operator *(Polynomial value1, Polynomial value2)
+        {
+            if (value1 == null || value2 == null)
+                return null;
+
+            double[] resultCoefficients = new double[value1.power + 1 + value2.power + 1];
+
+            for (int i = 0; i <= value1.power; i++)
+                for (int j = 0; i <= value2.power; j++)
+                    resultCoefficients[i + j] = value1.coefficients[i] * value2.coefficients[j];
+
+            return new Polynomial(resultCoefficients);
+        }  
+    }
+}
